8aadce053023a3750fec0945db0a251409660aef
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2022-08-30 02:33:04 +0200

    implement notification component (#8)
    * implement notification component
    
    * add notification property for Robot
    
    * Update Notification file name in header comment
    
    * Fix: Notification async-await issues, add new method for requesting notification authorization if needed

---
+++ Sources/RobotKit/Actions/Notification.swift
@@ -0,0 +1,71 @@
+//
+//  Notification.swift
+//  RobotKit
+//
+//  Created by Ahmed Shendy on 29/08/2022.
+//
+
+import Foundation
+import UserNotifications
+
+/// Robot control of notification center and its delegate
+public actor RobotNotification: SelfTasking {
+    
+    private class DefaultNotificationCenterDelegate: NSObject, UNUserNotificationCenterDelegate { }
+    
+    private var current: UNUserNotificationCenter = UNUserNotificationCenter.current()
+    
+    public var delegate: UNUserNotificationCenterDelegate = DefaultNotificationCenterDelegate() {
+        didSet {
+            current.delegate = delegate
+        }
+    }
+    
+    init() {
+        current.delegate = delegate
+    }
+    
+    /// Request notification permission from user if needed
+    @discardableResult
+    public func requestAuthorizationIfNeeded(options: UNAuthorizationOptions = [.alert, .sound]) async -> Bool {
+        guard await getAuthorizationStatus() != .authorized else {
+            return await requestAuthorization(options: options)
+        }
+        
+        return true
+    }
+    
+    /// Request notification permission from user
+    @discardableResult
+    public func requestAuthorization(options: UNAuthorizationOptions = [.alert, .sound]) async -> Bool {
+        do {
+            return try await current.requestAuthorization(options: options)
+        } catch {
+            return false
+        }
+    }
+    
+    /// Send notification message to user
+    public func push(title: String, subtitle: String?, message: String) async throws {
+        guard await getAuthorizationStatus() == .authorized else { return }
+        
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = message
+        if let subtitle = subtitle {
+            content.subtitle = subtitle
+        }
+        
+        let request = UNNotificationRequest(
+            identifier: UUID().uuidString,
+            content: content,
+            trigger: nil
+        )
+        
+        try await current.add(request)
+    }
+    
+    public func getAuthorizationStatus() async -> UNAuthorizationStatus {
+        return await current.notificationSettings().authorizationStatus
+    }
+}
